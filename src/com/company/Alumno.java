7fbf1306518b5731ee59f06c0ba406d99a2efcb6
jp <juan.ghelfi@tictapps.com>
2018-03-12 18:49:23 -0300

    wip

---
+++ src/com/company/Alumno.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Alumno {
+
+    protected String nombre;
+
+    protected String apellido;
+
+    protected Integer codigoAlumno;
+
+    public Alumno(String nombre, String apellido, Integer codigoAlumno) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.codigoAlumno = codigoAlumno;
+    }
+
+    
+}
